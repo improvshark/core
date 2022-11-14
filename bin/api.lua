583fd48fd455596fc6b9a695f2a9959385a7f523
improvshark <improvshark@gmail.com>
2022-11-13 17:16:14 -0700

    refactor api to use argparse

--- bin/api.lua
+++ bin/api.lua
@@ -1,5 +1,3 @@
-
-
 local vocal = true;
 
 function isApi(path)
@@ -9,9 +7,9 @@ end
 local function loadApi(path)
 	if isApi(path) then
 		if os.loadAPI(path) and vocal then
-   			print('+ loaded: ' .. fs.getName(path) )
-   			return true
-   		end
+			print('+ loaded: ' .. fs.getName(path))
+			return true
+		end
 	else
 		return false
 	end
@@ -19,11 +17,12 @@ end
 
 local function unloadApi(path)
 	if isApi(path) then
-		os.unloadAPI(fs.getName(path))
-	 	if vocal then
-   			print('~ unloaded: ' .. fs.getName(path) )
-   			return true
-   		end
+		local name = string.gsub(fs.getName(path), '.lua$', '')
+		os.unloadAPI(name)
+		if vocal then
+			print('~ unloaded: ' .. fs.getName(path))
+			return true
+		end
 	else
 		return false
 	end
@@ -31,19 +30,19 @@ end
 
 local function load(path, recursive)
 
-	if  fs.exists(path) == false  then
+	if fs.exists(path) == false then
 		print('file or path does not exist')
 		return false
 	end
 
 	if fs.isDir(path) then
 		local table = fs.list(path)
-		for i=1,#table do
-   			if recursive == true and fs.isDir(table[i]) then
-   				load(fs.combine(path, table[i]), true)
-   			else
-   				loadApi(fs.combine(path, table[i]) )
-   			end
+		for i = 1, #table do
+			if recursive == true and fs.isDir(table[i]) then
+				load(fs.combine(path, table[i]), true)
+			else
+				loadApi(fs.combine(path, table[i]))
+			end
 		end
 	else
 		loadApi(path)
@@ -52,46 +51,45 @@ end
 
 local function unload(path, recursive)
 
-	if  fs.exists(path) == false  then
+	if fs.exists(path) == false then
 		print('file or path does not exist')
 		return false
 	end
 
 	if fs.isDir(path) then
 		local table = fs.list(path)
-		for i=1,#table do
-   			if recursive == true and fs.isDir(table[i]) then
-   				unload(fs.combine(path, table[i]), true)
-   			else
-   				unloadApi(fs.combine(path, table[i]) )
-   			end
+		for i = 1, #table do
+			if recursive == true and fs.isDir(table[i]) then
+				unload(fs.combine(path, table[i]), true)
+			else
+				unloadApi(fs.combine(path, table[i]))
+			end
 		end
 	else
 		unloadApi(path)
 	end
 end
 
-local arg = {...}
+local arg = { ... }
 
 local function main()
 
-	if arg[1] == nil then
-		print("api <load/unload> <path/file> ")
-	end
+	local argparse = require("argparse")
+	local parser = argparse("api", "load an api or folder of apis")
+	parser:command("list")
+	parser:flag('-r --recursive')
+	parser:command("load"):argument('path')
+	parser:command("unload"):argument('path')
+	local args = parser:parse(arg)
+	local recursive = args['recursive'] or false
 
-	if arg[1] == "load" and arg[2] ~= nil then
-		load(shell.resolve(arg[2]), false)
-	end
-
-	if arg[1] == "unload" and arg[2] ~= nil then
-		unload(shell.resolve(arg[2]), false)
-	end
-
-	if arg[1] == "list" then
+	if args['load'] then
+		load(shell.resolve(args['path']), recursive)
+	elseif args['unload'] then
+		unload(shell.resolve(args['path']), recursive)
+	elseif args['list'] then
 		shell.run('apis')
 	end
-
 end
-
 
 main()
